db36c068e314c61c21fab0e67a829b6759c37c2b
Emmanuel Buckley <ebuckley2389@gmail.com>
2022-01-29 23:37:37 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  const token = await core.getIDToken();
+  // const octokit = github.getOctokit(token, {baseUrl: 'https://api.github.com'});
+  const octokit = github.getOctokit(token);
+
+  const { data } = await octokit.rest.pulls.get({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: github.context.issue.number
+  });
+
+  console.log({ data });
+}
+try {
+  const pathToVersion = core.getInput('version-json-path')
+  console.log(`Path to version.json: ${pathToVersion}`);
+  const repoUrl = `https://api.github.com/repos/${github.context.repo.repo}/pulls/${github.context.issue.number}/files`
+  console.log({repoUrl})
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log({payload});
+  core.setOutput('time', (new Date()).toTimeString()); // temporary for testing
+  run();
+} catch (error) {
+  core.setFailed(error.message);
+}
