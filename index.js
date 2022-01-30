de5974f738b3e91c7d91954f3d1882a8256a945c
Emmanuel Buckley <ebuckley2389@gmail.com>
2022-01-30 00:21:45 -0600

    read pr files

--- index.js
+++ index.js
@@ -6,21 +6,24 @@ async function run() {
   // const octokit = github.getOctokit(token, {baseUrl: 'https://api.github.com'});
   const octokit = github.getOctokit(token);
 
-  const { data } = await octokit.rest.pulls.get({
+  const { data } = await octokit.rest.pulls.listFiles({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
     pull_number: github.context.issue.number
   });
+
+  const version_json = data.find(x => x.filename == 'version.json');
+  console.log({version_json})
 
   console.log({ data });
 }
 try {
   const pathToVersion = core.getInput('version-json-path')
   console.log(`Path to version.json: ${pathToVersion}`);
-  const repoUrl = `https://api.github.com/repos/${github.context.repo.repo}/pulls/${github.context.issue.number}/files`
-  console.log({repoUrl})
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log({payload});
+  // const repoUrl = `https://api.github.com/repos/${github.context.repo.repo}/pulls/${github.context.issue.number}/files`
+  // console.log({repoUrl})
+  // const payload = JSON.parse(github.context.payload, undefined, 2);
+  // console.log({payload});
   core.setOutput('time', (new Date()).toTimeString()); // temporary for testing
   run();
 } catch (error) {
